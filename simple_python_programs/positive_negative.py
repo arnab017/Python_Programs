2e0b4c7345b124df6a0bd8a247e41081a08ed5fa
Arnab Acharya <134071381+arnab017@users.noreply.github.com>
2023-06-01 22:00:23 +0530

    Add files via upload

---
+++ simple_python_programs/positive_negative.py
@@ -0,0 +1,10 @@
+
+#^ Python Program to Check Whether a Number is Positive or Negative
+
+n = int(input('Enter a number : '))
+if n > 0:
+    print(n,'is positive')
+elif n < 0:
+    print(n,'is negative')
+else:
+    print('Invalid')
