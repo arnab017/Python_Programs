c4511151e7246a6bebbf76cd756b162bc5326a6b
Arnab Acharya <134071381+arnab017@users.noreply.github.com>
2023-06-01 22:28:18 +0530

    Add files via upload

---
+++ simple_python_programs/reverse_number.py
@@ -0,0 +1,11 @@
+
+#^ Python Program to Reverse a Number
+
+num = int(input('Enter a number : '))
+n = num
+rev = 0
+while n > 0:
+    rem = n%10
+    rev = rev*10 + rem
+    n = n//10
+print('reverse of',num,'is',rev)
